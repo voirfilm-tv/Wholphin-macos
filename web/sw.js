42a2a77e102cc05c31a1f7adc6b482448b538048
gWEN4.2 <82654068+voirfilm-tv@users.noreply.github.com>
2026-07-19 18:27:48 +0200

    Add service worker

---
+++ web/sw.js
@@ -0,0 +1,12 @@
+const CACHE = 'wholphin-web-v1';
+const SHELL = ['./', './index.html', './styles.css', './js/app.js', './js/api.js', './js/demo.js', './js/player.js', './js/store.js', './assets/icon.svg', './manifest.webmanifest'];
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL))));
+self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))));
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE).then(cache => cache.put(event.request, copy));
+    return response;
+  }).catch(() => caches.match(event.request).then(cached => cached || caches.match('./index.html'))));
+});
